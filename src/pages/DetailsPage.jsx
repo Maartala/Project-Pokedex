2a1ecd9253584c038653d479c984c258c7daeb75
Jens Koetschau <jenscode@AirvonSperCoder.fritz.box>
2023-04-20 14:20:10 +0200

    location work on detailpage

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,7 +1,13 @@
+import { useLocation } from 'react-router-dom';
+
 const DetailsPage = () => {
+
+	const location = useLocation()
+	console.log(location)
+
 	return (
 		<div>
-
+			<p>{location.state.name}</p>
 		</div>
 	);
 };
