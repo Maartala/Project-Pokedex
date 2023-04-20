f08b4541e2aceb5f2b4f6b0d2332c58505b91996
AnaFern2023 <anafern2023@gmail.com>
2023-04-20 13:39:59 +0200

    PokemonList.jsx and PokemonListItem.jsx added

--- src/components/PokemonListItem.jsx
+++ src/components/PokemonListItem.jsx
@@ -1,12 +1,33 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const PokemonListItem = (props) => {
 
+	const [pic, setPic] = useState([])
+	const [id, setId] = useState([])
+	const secondFetch = props.url
+
+	// console.log(props);
+
+	useEffect(() => {
+		fetch(secondFetch)
+			.then(response => response.json())
+			.then(json => {
+				// console.log(json);
+				setPic(json.sprites.other.dream_world.front_default)
+				setId(json.id)
+				// console.log(json.id);
+			})
+	})
+	// console.log(pic);
+	// console.log(id);
 	return (
 		<div>
+			<h2>#{id}</h2>
 			<Link to={`/pokemon/${props.pokeId}`}>
-				Pokemon
+				<img src={pic} alt={props.name} />
 			</Link>
+			<h2>{props.name}</h2>
 		</div>
 	);
 };
