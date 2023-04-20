525b3d711b6cc926eb71e8429cc313a54416250b
Justin Schildt <Just.schildt@gmail.com>
2023-04-20 15:46:31 +0200

    Added PokemonType side menu

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,16 +1,19 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import PokemonList from "../../components/PokemonList/PokemonList.jsx";
 import {Link} from "react-router-dom";
 
-import styles from "./main-page.module.scss";
+import styles from "./mainPage.module.scss";
 
 import pokemonTitle from "../../assets/img/pokemon-title.png";
 import hamburgerMenuIcon from "../../assets/img/icons/hamburger-menu-icon.svg";
 import lightThemeIcon from "../../assets/img/icons/light-theme-icon.svg";
 import darkThemeIcon from "../../assets/img/icons/dark-theme-icon.svg";
+import PokemonTypes from "../../components/PokemonTypes/PokemonTypes.jsx";
 
 
 const MainPage = ({isDarkModeEnabled, setIsDarkModeEnabled}) => {
+
+	const [isMenuExpanded, setIsMenuExpanded] = useState(false);
 
 	useEffect(() => {
 		const app = document.getElementById("app");
@@ -24,6 +27,19 @@ const MainPage = ({isDarkModeEnabled, setIsDarkModeEnabled}) => {
 		}
 	}, [isDarkModeEnabled]);
 
+	useEffect(() => {
+		const typeMenu = document.getElementById("typeMenu");
+		if(isMenuExpanded){
+			typeMenu.style.left = "0";
+		}
+		else {
+			typeMenu.style.left = "-80vw";
+		}
+	})
+
+	const expandTypeMenu = () => {
+		setIsMenuExpanded(!isMenuExpanded)
+	}
 	const toggleTheme = () => {
 		setIsDarkModeEnabled(!isDarkModeEnabled);
 	};
@@ -33,7 +49,7 @@ const MainPage = ({isDarkModeEnabled, setIsDarkModeEnabled}) => {
 			<header>
 				<img src={pokemonTitle} alt="Pokemon"/>
 				<form action="#" onSubmit={(event) => event.preventDefault()}>
-					<button>
+					<button onClick={expandTypeMenu}>
 						<img src={hamburgerMenuIcon} alt="Menu"/>
 					</button>
 					<input type="text"/>
@@ -42,6 +58,9 @@ const MainPage = ({isDarkModeEnabled, setIsDarkModeEnabled}) => {
 					</button>
 				</form>
 			</header>
+			<aside id="typeMenu">
+				<PokemonTypes/>
+			</aside>
 
 			<Link to={"/pokemon/types"}>
 				Steffis Pokemon Types
