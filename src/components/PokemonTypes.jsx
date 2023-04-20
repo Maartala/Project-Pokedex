5d42eb42abbab9433ff8d9df897e6c3a7c9e2fe8
Maartala <Steffi.Gerstenmaier@t-online.de>
2023-04-20 13:41:28 +0200

    PokemonTypes 1st upload

--- src/components/PokemonTypes.jsx
+++ src/components/PokemonTypes.jsx
@@ -1,9 +1,46 @@
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import styles from './PokemonTypes.module.css';
+
+
 const PokemonTypes = () => {
+
+	const [allTypes, setAllTypes] = useState([]);
+	const [input, setInput] = useState("");
+
+	useEffect(() => {
+		fetch(`https://pokeapi.co/api/v2/type/${input}`)
+			.then(res => res.json())
+			.then(data => setAllTypes(data.results))
+	}, [input])
+
+	const handleSearch = (event) => {
+		const selectedValue = event.target.value;
+		setInput(selectedValue);
+	};
+
+
+	console.log(allTypes);
+
 	return (
-		<div>
+		<div className={styles.pokemontypes}>
+			<h2>TYPE</h2>
+			<form action="" onSubmit={(() => preventDefault())}>
+				{allTypes.map((elt) => {
+					return (
+						<Link to={`/type/${elt.id}`}>{elt.name}</Link>
+					)
+				})}
+
+			</form>
 
 		</div>
 	);
-};
+
+}
 
 export default PokemonTypes;
+
+
+
+
