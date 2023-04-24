1ca8da3cc85948cdcf5419fe529fb85921bb2d0f
Justin Schildt <Just.schildt@gmail.com>
2023-04-24 11:30:03 +0200

    Refactor MainPage component and add FilterContext to handle search filter with useState hook.

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 
 import NavBar from "../../components/navigation/NavBar.jsx";
@@ -9,70 +9,85 @@ import hamburgerMenuIcon from "../../assets/img/icons/hamburger-menu-icon.svg";
 import lightThemeIcon from "../../assets/img/icons/light-theme-icon.svg";
 import darkThemeIcon from "../../assets/img/icons/dark-theme-icon.svg";
 
+export const FilterContext = createContext("");
+
 const MainPage = ({ isDarkModeEnabled, setIsDarkModeEnabled }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
 
-	const location = useLocation();
-	const navigate = useNavigate();
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("/pokemon");
+    }
+  }, []);
 
-	useEffect(() => {
-		if (location.pathname === "/") {
-			navigate("/pokemon");
-		}
-	}, []);
+  useEffect(() => {
+    const app = document.getElementById("app");
+    if (isDarkModeEnabled) {
+      app.classList.remove("light");
+      app.classList.add(styles.dark);
+    } else {
+      app.classList.remove(styles.dark);
+      app.classList.add("light");
+    }
+  }, [isDarkModeEnabled]);
+  const toggleTheme = () => {
+    setIsDarkModeEnabled(!isDarkModeEnabled);
+  };
 
+  const [isMenuExpanded, setIsMenuExpanded] = useState(false);
+  useEffect(() => {
+    document.getElementById("typeMenu").style.left = isMenuExpanded
+      ? "0"
+      : "-500px";
+  }, [isMenuExpanded]);
 
-	useEffect(() => {
-		const app = document.getElementById("app");
-		if (isDarkModeEnabled) {
-			app.classList.remove("light");
-			app.classList.add(styles.dark);
-		} else {
-			app.classList.remove(styles.dark);
-			app.classList.add("light");
-		}
-	}, [isDarkModeEnabled]);
-	const toggleTheme = () => {
-		setIsDarkModeEnabled(!isDarkModeEnabled);
-	};
+  const handleMenuBack = (event) => {
+    if (
+      isMenuExpanded &&
+      !document.getElementById("typeMenu").contains(event.target)
+    ) {
+      setIsMenuExpanded(false);
+    }
+  };
 
-	const [isMenuExpanded, setIsMenuExpanded] = useState(false);
-	useEffect(() => {
-		document.getElementById("typeMenu").style.left = isMenuExpanded ? "0" : "-500px";
-	}, [isMenuExpanded]);
+  const [filter, setFilter] = useState("");
+  const handleSearch = (event) => {
+    setFilter(event.target.value);
+  };
 
-	const handleMenuBack = (event) => {
-		if (isMenuExpanded && !document.getElementById("typeMenu").contains(event.target)){
-			setIsMenuExpanded(false);
-		}
-	}
+  return (
+    <div
+      id="mainPage"
+      className={[styles.mainPage].join(" ")}
+      onClick={handleMenuBack}
+    >
+      <header>
+        <Link to="/pokemon">
+          <img src={pokemonTitle} alt="Pokemon" />
+        </Link>
+        <section>
+          <button onClick={() => setIsMenuExpanded(!isMenuExpanded)}>
+            <img src={hamburgerMenuIcon} alt="Menu" />
+          </button>
+          <input type="text" onChange={handleSearch} />
+          <button onClick={toggleTheme} id="toggleThemeButton">
+            <img
+              src={isDarkModeEnabled ? lightThemeIcon : darkThemeIcon}
+              alt="theme-toggle"
+            />
+          </button>
+        </section>
+      </header>
 
-	const handleSearch = (event) => {
-		console.log(event.target.value)
-	}
-
-	return (
-		<div id="mainPage" className={[styles.mainPage].join(" ")} onClick={handleMenuBack}>
-			<header>
-				<Link to="/pokemon">
-					<img src={pokemonTitle} alt="Pokemon" />
-				</Link>
-				<section>
-					<button onClick={() => setIsMenuExpanded(!isMenuExpanded)}>
-						<img src={hamburgerMenuIcon} alt="Menu" />
-					</button>
-					<input type="text" onChange={handleSearch} />
-					<button onClick={toggleTheme} id="toggleThemeButton">
-						<img src={isDarkModeEnabled ? lightThemeIcon : darkThemeIcon} alt="theme-toggle" />
-					</button>
-				</section>
-			</header>
-
-			<aside id="typeMenu">
-				<NavBar setMenuState={setIsMenuExpanded}/>
-			</aside>
-			<Outlet />
-		</div>
-	);
+      <aside id="typeMenu">
+        <NavBar setMenuState={setIsMenuExpanded} />
+      </aside>
+      <FilterContext.Provider value={filter}>
+        <Outlet />
+      </FilterContext.Provider>
+    </div>
+  );
 };
 
 export default MainPage;
